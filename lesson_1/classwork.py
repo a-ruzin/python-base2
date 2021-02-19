8080a9d728487a53d1126f52a1450259811bba7f
Alexey Ruzin <ruzin@kokoc.com>
2021-02-19 19:46:14 +0300

    добавлен демонстрационный if

--- lesson_1/classwork.py
+++ lesson_1/classwork.py
@@ -12,8 +12,10 @@ leg_pairs = [
 ]
 
 for legs in leg_pairs:
-    h = calc_hypotenuse(legs[0], legs[1])
-    print(f'Гипотенуза равна {h:.2f} для треугольника с катетами: {legs[0]:.2f} и {legs[1]:.2f}')
-
+    if len(legs) == 2:
+        h = calc_hypotenuse(legs[0], legs[1])
+        print(f'Гипотенуза равна {h:.2f} для треугольника с катетами: {legs[0]:.2f} и {legs[1]:.2f}')
+    else:
+        print('Неверное число катетов для треугольника')
 
 #  переменная, тип переменной, оператор ветвления, цикл, функция
