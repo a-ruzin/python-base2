19492caed55705d2e13d9688a95f9b69249a3793
Alexey Ruzin <ruzin@kokoc.com>
2021-02-26 19:44:44 +0300

    class Cat

---
+++ lesson_2/classwork.py
@@ -0,0 +1,26 @@
+# class Cat:
+#     name = None
+#     color = None
+#
+#     def describe(self):
+#         print(f'{self.name} is {self.color}')
+#
+#
+# cat = Cat()
+# cat.name = 'Marusya'
+# cat.color = 'white'
+# cat.describe()
+#
+# cat2 = Cat()
+# cat2.name = 'Barsik'
+# cat2.color = 'black'
+# cat2.describe()
+#
+# cat.describe()
+#
+# other_cat = cat
+# other_cat.describe()
+#
+# other_cat.color = 'red'
+# other_cat.describe()
+# cat.describe()
