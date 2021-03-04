ac0d2588c385c3431e851c77e22ee1ee41b3b5a6
Alexey Ruzin <ruzin@kokoc.com>
2021-03-05 00:27:35 +0300

    task1

--- lesson_3/homework/task1.py
+++ lesson_3/homework/task1.py
@@ -1,3 +1,33 @@
 """
+1. Написать функцию num_translate(), переводящую числительные от 0 до 10 c английского
+на русский язык. Например:
+>>> num_translate("one")
+'один'
+>>> num_translate("eight")
+'восемь'
+>>> num_translate("eleven")
 
+Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить
+информацию, необходимую для перевода: какой тип данных выбрать, в теле функции или снаружи.
 """
+
+dictionary = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять',
+}
+
+
+def num_translate(word):
+    return dictionary.get(word)
+
+
+print(num_translate('one'))
+print(num_translate('two'))
